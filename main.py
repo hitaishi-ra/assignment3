6936fae6e7286321e2d0df085307b50abccb3e64
hitaishi-ra <71496870+hitaishi-ra@users.noreply.github.com>
2020-09-24 01:24:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+"""
+
+1)	Write a program to give values to two variables and check if it is a square or rectangle.
+
+"""
+a=16
+if (a==15):
+    print("square")
+else:
+    print("rectangle")
+
+
+"""
+2)	Write a program to initiate two variables and store an integer value in them. Using proper loop print whether the first number is greater, or the second number is greater or both are equal.
+"""
+a=40
+b=50
+if(a>b):
+    print("greater")
+elif(a<b):
+    print("smaller")
+else:
+    print("equal")
+
+
+"""
+
+
+3)	Write a program to define age of three people and determine oldest and youngest among them.
+"""
+
+
+
+"""
+
+
+
+
+
+
+4)	Write a program to assign value to a variable and then check if the number is even or odd.
+"""
+
+num = 7
+if (num % 2) == 0:
+    print("7 is Even number")
+else:
+    print("7 is Odd number")
+
+
+"""
+
+
+
+5)	Mary wants to eat chocolate but she can only do that if it doesn’t rain. Otherwise she has to eat ice cream. Write a program to assign a statement “It is raining” to a variable and then decide if Mary should ice cream or chocolate?
+
+"""
+
+a='it is raining'
+if("a"):
+    print("chocolate")
+else:
+    print("icecream")
+
+
+"""
+
+
+"""
+
